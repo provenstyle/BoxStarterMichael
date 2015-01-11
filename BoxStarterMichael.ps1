462f46d6e8988a3242bd58e50e0cc0f6c3da83f6
Michael Dudley <michael.dudley@improvingenterprises.com>
2015-01-11 16:29:23 -0600

    Initial commit

---
+++ BoxStarterMichael.ps1
@@ -0,0 +1,25 @@
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+Enable-RemoteDesktop
+
+cinst googlechrome
+cinst sublimetext3
+cinst launchy
+cinst FoxitReader
+cinst sysinternals
+cinst git
+cinst logexpert
+cinst baretail
+cinst fiddler
+cinst linqpad4
+cinst filezilla
+cinst zoomit
+cinst putty
+cinst vim
+cinst poshgit
+cinst 7zip
+cinst vlc
+cinst nodejs
+cinst resharper
+cinst githubforwindows
+cinst markdownpad2
+cinst conemu
