e9d206cea188d19f471e8c34c2c7581ef8a95e29
Michael Dudley <michael.dudley@improving.com>
2017-11-22 15:13:41 -0600

    updating

--- BoxStarterMichael.ps1
+++ BoxStarterMichael.ps1
@@ -1,27 +1,27 @@
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 Enable-RemoteDesktop
 
-cinst googlechrome
+cinst timrayburn.gitaliases
 cinst sublimetext3
-cinst cmder
 cinst launchy
 cinst FoxitReader
 cinst sysinternals
-cinst git
-cinst logexpert
-cinst baretail
 cinst fiddler
-cinst linqpad4
 cinst filezilla
 cinst putty
 cinst vim
-cinst poshgit
 cinst 7zip
-cinst vlc
-cinst nodejs
-cinst resharper
-cinst githubforwindows
 cinst markdownpad2
-cinst conemu
-cinst timrayburn.gitaliases
-cinst beyondcompare
+#cinst googlechrome
+#cinst cmder
+#cinst git
+#cinst logexpert
+#cinst baretail
+#cinst linqpad4
+#cinst poshgit
+#cinst vlc
+#cinst nodejs
+#cinst resharper
+#cinst githubforwindows
+#cinst conemu
+#cinst beyondcompare
